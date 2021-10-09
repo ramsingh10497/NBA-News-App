d6fa5806ee9f1fcadb02049043dc96b367f40d65
Ram Singh <rams48705@gmail.com>
2021-10-09 18:27:37 +0530

    ArticlesList

---
+++ src/components/Articles/News/Post/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+export default Header
--- src/components/Articles/News/Post/NewsArticles.jsx
+++ src/components/Articles/News/Post/NewsArticles.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
 import axios from 'axios';
 import styles from '../../NewsArticles.css';
+import Header from './Header';
+import Body from './Body';
 
 
 const NewsArticles = (props) => {
     const [articles, setArticles] = React.useState([]);
     const [team, setTeam] = React.useState([]);
+
+    
+    
     const request = () => {
             
         axios.get(`http://localhost:8000/articles?id=${props.match.params.id}`)
             .then(res => {
+                
                 setArticles(...res.data)
+                let temp = res.data[0];
+               
+                
+                axios.get(`http://localhost:8000/teams?id=${temp.team}`)
+                    .then(res => {
+               
+                        setTeam(...res.data)
+                    })
             })
-
-        axios.get(`http://localhost:8000/teams?id=${articles.team}`)
-        .then(res => setTeam(res.data))
     }
 
     React.useEffect(() => {
         request()
     }, [])
 
-    
     return (
         <div>
-            NewsArticles
+            <Header
+                teamdata={team}
+                date={articles.date}
+                author={articles.author}
+            />
+            <Body />
         </div>
     )
 }
